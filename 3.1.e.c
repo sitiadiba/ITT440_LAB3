e2828c2f7cf7ff0eef65e6beb97296fc4351ab8f
sitiadiba <ctadiba007@gmail.com>
2021-04-30 05:44:50 +0000

    commit for 3.1

---
+++ 3.1.e.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <signal.h>
+
+int main (void)
+{
+void sigtstp_handler(int sig);
+char s [200];
+
+if (signal(SIGTSTP, sigtstp_handler)==SIG_ERR){
+perror("signal");
+exit(1);
+}
+
+printf("Enter a string:\n");
+if(fgets(s,200,stdin)==NULL)
+perror("gets");
+
+else
+printf("You entered:%s\n",s);
+
+return 0;
+
+}
+
+void sigtstp_handler (int sig)
+{
+printf("This is a special signal handler for sigtstp!\n");
+}
